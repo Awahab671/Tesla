2c72b837045ab044e9a4d65f737c58a7647e4a90
Wahab <88748641+Awahab671@users.noreply.github.com>
2021-11-08 15:42:58 -0800

    This is the first tesla model

---
+++ com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ModelS modelS = new ModelS();
+        modelS.Doors=4;
+        modelS.Engine=2;
+        modelS.Speed= 280;
+
+
+    }
+}
---
+++ com/company/ModelS.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class ModelS {
+    int Doors;
+    int Engine;
+    int Speed;
+}
